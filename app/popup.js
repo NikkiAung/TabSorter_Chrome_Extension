ebc0126014d72306d76b0f394b606195b1eb397b
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-03-26 00:00:46 -0700

    develop: minimum viable product chrome extension successfully

--- app/popup.js
+++ app/popup.js
@@ -1,18 +1,23 @@
 function byAlphabeticalURLOrder(tab1, tab2) {
-    if (tab1.url < tab2.url) {
-      return -1;
-    } else if (tab1.url > tab2.url) {
-      return 1;
+    const cleanURL = (url) => url.replace(/^https?:\/\/(www\.)?/, '');
+    const url1 = cleanURL(tab1.url);
+    const url2 = cleanURL(tab2.url);
+
+    if (url1 < url2) {
+        return -1;
+    } else if (url1 > url2) {
+        return 1;
     }
     return 0;
 }
-  
+
 chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
     tabs.sort(byAlphabeticalURLOrder);
     document.write(`<h3>The tabs you're on are:</h3>`);
     document.write('<ul>');
     for (let i = 0; i < tabs.length; i++) {
-      document.write(`<li>${tabs[i].url}</li>`);
+        document.write(`<li>${tabs[i].url}</li>`);
+        chrome.tabs.move(tabs[i].id, {index: i});
     }
     document.write('</ul>');
 });
